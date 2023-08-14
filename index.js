b6e18b4771cb354d35c2bfab0611bea621e7b771
Replit user <>
2023-08-14 21:24:32 +0000

    update

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//EJS
+app.set("view engine","ejs");
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  let title = "Home";
+  res.render("pages/index",{"title":title});
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.get('/about', (req, res) => {
+  let title = "About";
+  res.render("pages/about",{"title":title});
+});
+
+app.get('/projects', (req, res) => {
+  let title = "Projects";
+  res.render("pages/projects",{"title":title});
+});
+
+app.get('/contact', (req, res) => {
+  let title = "Contact";
+  res.render("pages/contact",{"title":title});
+});
+
+app.get('/facts', (req, res) => {
+  let title = "Facts about me";
+  res.render("pages/facts",{"title":title});
+});
